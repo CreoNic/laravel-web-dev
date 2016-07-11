51879094233793c3d6201402e71024f9fd96d9ae
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-11 22:56:27 +0200

    Added line and row for list of blogs and a sidebar

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -66,6 +66,51 @@
           </div>
         </div>
       </div> <!-- end of header .row -->
+
+      <div class="row">
+        <div class="col-md-8">
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+
+          <hr>
+
+          <div class="post">
+            <h3>Post Title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <a href="#" class="btn btn-primary">Read More</a>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="col-md-3 col-md-offset-1">
+          <h2>Sidebar</h2>
+        </div>
+      </div>
+
     </div> <!-- end of .container -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
