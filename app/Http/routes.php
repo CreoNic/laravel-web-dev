6feb787afdbcc2322fc8e8273fe31570179f40dc
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-24 18:04:58 +0200

    Fixed routes to email reset and created views for password resets

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,7 +20,7 @@ Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 // Password Reset Routes
-Route::get('password/reset({token?})', 'Auth\PasswordController@showResetForm');
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
 Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
 Route::post('password/reset', 'Auth\PasswordController@reset');
 
