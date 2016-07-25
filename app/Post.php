059b4d33a9c991c099fca624ff93f432d487e060
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-25 14:14:05 +0200

    Created Category Model relations to and from Post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Relationalship belongs to
+    public function category()
+    {
+      return $this->belongsTo('App\Category');
+    }
 }
