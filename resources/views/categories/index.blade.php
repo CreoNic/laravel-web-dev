0aeab67548eb4c01df0f080efc20bdb91af89ce6
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-26 18:37:43 +0200

    Added category routs and index view to show our categories

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,27 @@
+@extends('main')
+@section('title', '| All Categories')
+@section('content')
+
+<div class="row">
+  <div class="col-md-8">
+    <h1>Categories</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach ($categories as $category )
+        <tr>
+          <th>{{ $category->id }}</th>
+          <td>{{ $category->name }}</td>
+        </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+@endsection
