ffec74fb0674e9b6e6f77c1a9c99392758b1c225
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-26 18:54:12 +0200

    Added submit button to index category page.

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -22,6 +22,18 @@
       @endforeach
       </tbody>
     </table>
+  </div> <!-- end of .col-md-8 -->
+
+  <div class="col-md-3">
+    <div class="well">
+      {{ Form::open(['route' => 'categories.store', 'method' => 'POST']) }}
+        <h2>New Category</h2>
+        {{ Form::label('name', 'Name') }}
+        {{ Form::text('name', null, ['class' => 'form-control']) }}
+
+        {{ Form::submit('Create New Category', ['class' => 'btn btn-primary btn-block btn-h1-spacing']) }}
+      {{ Form::close() }}
+    </div>
   </div>
 </div>
 @endsection
