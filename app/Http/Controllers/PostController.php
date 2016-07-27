6a9bc174d134bb77e1c708ba9c91a21c38f83667
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-27 16:52:17 +0200

    Added update functionallity for categories

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -96,9 +96,15 @@ class PostController extends Controller
     {
         // find the post in the datbase and save as a var
         $post = Post::find($id);
+        $categories = Category::all();
+
+        $cats = array();
+        foreach($categories as $category){
+          $cats[$category->id] = $category->name;
+        }
 
         // return the view and pass in the var we previously created
-        return view('posts.edit')->withPost($post);
+        return view('posts.edit')->withPost($post)->withCategories($cats);
     }
 
     /**
@@ -116,12 +122,14 @@ class PostController extends Controller
         $this->validate($request, array(
           'title' => 'required|max:255',
           'slug' => 'required|alpha_dash|min:5|max:255',
+          'category_id' => 'required|integer',
           'body' => 'required'
         ));
       }else{
         $this->validate($request, array(
           'title' => 'required|max:255',
           'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+          'category_id' => 'required|integer',
           'body' => 'required'
         ));
       }
@@ -131,7 +139,8 @@ class PostController extends Controller
       $post = Post::find($id);
 
       $post->title = $request->input('title');
-      $post->slug = $request->slug;
+      $post->slug = $request->input('slug');
+      $post->category_id = $request->input('category_id');
       $post->body = $request->input('body');
 
       $post->save();
