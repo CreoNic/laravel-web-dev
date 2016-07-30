c8be5a51976acb130bb1fb1c0812a2329bf1f6d7
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-07-30 17:45:30 +0200

    Created tag delete functionality

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -6,8 +6,13 @@
     <div class="col-md-8">
       <h1>{{ $tag->name }} Tag <small>{{ $tag->posts()->count() }} Posts</small></h1>
     </div>
-    <div class="col-md-2 col-md-offset-2">
+    <div class="col-md-2">
       <a href="{{ route('tags.edit', $tag->id) }}" class="btn btn-primary pull-right btn-block edit-spacing-top">Edit</a>
+    </div>
+    <div class="col-md-2">
+      {{ Form::open(['route' => ['tags.destroy', $tag->id], 'method' => 'DELETE']) }}
+        {{ Form::submit('Delete', ['class' => 'btn btn-danger btn-block edit-spacing-top']) }}
+      {{ Form::close() }}
     </div>
   </div>
 
