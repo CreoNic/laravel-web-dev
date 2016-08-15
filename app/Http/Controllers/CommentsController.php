4fd153c2e608297d49fee43d9c605575977c3f68
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-08-15 20:57:59 +0200

    Added CRUD for comments

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -5,112 +5,101 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-
 use App\Comment;
 use App\Post;
 use Session;
 
 class CommentsController extends Controller
 {
-  //Add protection for all pages that uses PostController
-  //Only logged in users can access these pages.
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => 'store']);
+    }
 
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $post_id)
+    {
+        $this->validate($request, array(
+            'name'      =>  'required|max:255',
+            'email'     =>  'required|email|max:255',
+            'comment'   =>  'required|min:5|max:2000'
+            ));
 
-  }
+        $post = Post::find($post_id);
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
+        $comment = new Comment();
+        $comment->name = $request->name;
+        $comment->email = $request->email;
+        $comment->comment = $request->comment;
+        $comment->approved = true;
+        $comment->post()->associate($post);
 
-  }
+        $comment->save();
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request, $post_id)
-  {
-    $this->validate($request, array(
-      'name' => 'required|max:255',
-      'email' => 'required|email|max:255',
-      'comment' => 'required|min:5|max:2000'
-    ));
+        Session::flash('success', 'Comment was added');
 
-    $post = Post::find($post_id);
+        return redirect()->route('blog.single', [$post->slug]);
+    }
 
-    $comment = new Comment();
-    $comment->name = $request->name;
-    $comment->email = $request->email;
-    $comment->comment = $request->comment;
-    $comment->approved = true;
-    $comment->post()->associate($post);
-    $comment->save();
 
-    Session::flash('success', 'Comment was added.');
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $comment = Comment::find($id);
+        return view('comments.edit')->withComment($comment);
+    }
 
-    return redirect()->route('blog.single', [$post->slug]);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $comment = Comment::find($id);
 
-  }
+        $this->validate($request, array('comment' => 'required'));
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show($id)
-  {
+        $comment->comment = $request->comment;
+        $comment->save();
 
-  }
+        Session::flash('success', 'Comment updated');
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function edit($id)
-  {
+        return redirect()->route('posts.show', $comment->post->id);
+    }
 
-  }
+    public function delete($id)
+    {
+        $comment = Comment::find($id);
+        return view('comments.delete')->withComment($comment);
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $post_id = $comment->post->id;
+        $comment->delete();
 
-  }
+        Session::flash('success', 'Deleted Comment');
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-
-  }
+        return redirect()->route('posts.show', $post_id);
+    }
 }
