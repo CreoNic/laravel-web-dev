2701b14a4b04dc5c74622282f0319875eb168126
Niklas Wennerstrand <wennerstrand@gmail.com>
2016-08-20 19:55:09 +0200

    Added migration for image column in posts database

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -20,7 +20,7 @@
     <div class="col-md-8 col-md-offset-2">
       <h1>Create New Post</h1>
       <hr>
-      {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
+      {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '', 'files' => true)) !!}
         {{ Form::label('title', 'Title:')}}
         {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => 255)) }}
 
@@ -40,6 +40,9 @@
             <option value="{{ $tag->id }}">{{ $tag->name }}</option>
           @endforeach
         </select>
+
+        {{ Form::label('featured_image', 'Upload Featured Image:') }}
+        {{ Form::file('featured_image') }}
 
         {{ Form::label('body', "Post Body:") }}
         {{ Form::textarea('body', null, array('class' => 'form-control')) }}
